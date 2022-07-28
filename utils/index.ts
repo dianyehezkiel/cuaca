64a257371c632c0ac25e931d6f932534d931de47
dianyehezkiel <dianyehezkiel@gmail.com>
2022-07-29 00:35:39 +0700

    add utility function to convert between forecast data types

--- utils/index.ts
+++ utils/index.ts
@@ -1,2 +1,5 @@
 import _toCurrentWeather from "./toCurrentWeather";
 export const toCurrentWeather = _toCurrentWeather;
+
+import _toWeatherForecast from "./toWeatherForecast";
+export const toWeatherForecast = _toWeatherForecast;
