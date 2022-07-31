89219fa033e2ce1635a6ecf4352db0c3dd3cbb96
dianyehezkiel <dianyehezkiel@gmail.com>
2022-07-31 22:27:48 +0700

    update WeatherIcon component

--- components/WeatherIcon.tsx
+++ components/WeatherIcon.tsx
@@ -4,6 +4,7 @@ import RainDayIcon from "./icons/day/RainDayIcon"
 import RainMixDayIcon from "./icons/day/RainMixDayIcon"
 import ShowersDayIcon from "./icons/day/ShowersDayIcon"
 import SprinkleDayIcon from "./icons/day/SprinkleDayIcon"
+import StormShowersDayIcon from "./icons/day/StormShowersDayIcon"
 import ThunderstormDayIcon from "./icons/day/ThunderstormDayIcon"
 import LightningIcon from "./icons/LightningIcon"
 import LightningNightIcon from "./icons/night/LightningNightIcon"
@@ -11,11 +12,13 @@ import RainMixNightIcon from "./icons/night/RainMixNightIcon"
 import RainNightIcon from "./icons/night/RainNightIcon"
 import ShowersNightIcon from "./icons/night/ShowersNightIcon"
 import SprinkleNightIcon from "./icons/night/SprinkleNightIcon"
+import StormShowersNightIcon from "./icons/night/StormShowersNightIcon"
 import ThunderstormNightIcon from "./icons/night/ThunderstormNightIcon"
 import RainIcon from "./icons/RainIcon"
 import RainMixIcon from "./icons/RainMixIcon"
 import ShowersIcon from "./icons/ShowersIcon"
 import SprinkleIcon from "./icons/SprinkleIcon"
+import StormShowersIcon from "./icons/StormShowersIcon"
 import ThunderstormIcon from "./icons/Thunderstorm"
 
 interface WeatherIconProps extends IconProps {
@@ -48,16 +51,27 @@ export default function WeatherIcon(props: WeatherIconProps ) {
       case 300:
       case 301:
       case 321:
+      case 500:
         return <SprinkleIcon {...iconProps} />
       case 302:
       case 311:
       case 312:
       case 314:
+      case 501:
+      case 502:
+      case 503:
+      case 504:
         return <RainIcon {...iconProps} />
       case 310:
+      case 511:
         return <RainMixIcon {...iconProps} />
       case 313:
+      case 520:
+      case 521:
+      case 522:
         return <ShowersIcon {...iconProps} />
+      case 531:
+        return <StormShowersIcon {...iconProps} />
       default:
         // TODO: Change fallback icon
         return <Icon {...iconProps}/>
@@ -81,16 +95,27 @@ export default function WeatherIcon(props: WeatherIconProps ) {
       case 300:
       case 301:
       case 321:
+      case 500:
         return <SprinkleDayIcon {...iconProps} />
       case 302:
       case 311:
       case 312:
       case 314:
+      case 501:
+      case 502:
+      case 503:
+      case 504:
         return <RainDayIcon {...iconProps} />
       case 310:
+      case 511:
         return <RainMixDayIcon {...iconProps} />
       case 313:
+      case 520:
+      case 521:
+      case 522:
         return <ShowersDayIcon {...iconProps} />
+      case 531:
+        return <StormShowersDayIcon {...iconProps} />
       default:
         // TODO: Change fallback icon
         return <Icon {...iconProps}/>
@@ -112,16 +137,27 @@ export default function WeatherIcon(props: WeatherIconProps ) {
       case 300:
       case 301:
       case 321:
+      case 500:
         return <SprinkleNightIcon {...iconProps} />
       case 302:
       case 311:
       case 312:
       case 314:
+      case 501:
+      case 502:
+      case 503:
+      case 504:
         return <RainNightIcon {...iconProps} />
       case 310:
+      case 511:
         return <RainMixNightIcon {...iconProps} />
       case 313:
+      case 520:
+      case 521:
+      case 522:
         return <ShowersNightIcon {...iconProps} />
+      case 531:
+        return <StormShowersNightIcon {...iconProps} />
       default:
         // TODO: Change fallback icon
         return <Icon {...iconProps}/>
