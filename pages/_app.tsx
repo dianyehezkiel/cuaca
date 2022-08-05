eef23ffa817cc4f913bc9c4ae3b5143120debb28
dianyehezkiel <dianyehezkiel@gmail.com>
2022-08-05 22:32:41 +0700

    provide state for whole app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,15 @@ import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/globals.css'
 import Navbar from "../components/Navbar";
 import Footer from '../components/Footer';
+import { reducer, StateProvider } from '../state';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Navbar />
-      <Component {...pageProps} />
+      <StateProvider reducer={reducer}>
+        <Component {...pageProps} />
+      </StateProvider>
       <Footer />
     </ChakraProvider>
   )
