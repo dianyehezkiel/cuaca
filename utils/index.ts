bfecbe42eea2cdbd2da4a05144e32cbbdfa232eb
dianyehezkiel <dianyehezkiel@gmail.com>
2022-08-05 23:19:24 +0700

    refactor 2.0

--- utils/index.ts
+++ utils/index.ts
@@ -1,8 +1,8 @@
-import _toCurrentWeather from "./toCurrentWeather";
-export const toCurrentWeather = _toCurrentWeather;
+import _toCurrentWeather from "./toCurrentWeather"
+export const toCurrentWeather = _toCurrentWeather
 
-import _toWeatherForecast from "./toWeatherForecast";
-export const toWeatherForecast = _toWeatherForecast;
+import _toWeatherForecast from "./toWeatherForecast"
+export const toWeatherForecast = _toWeatherForecast
 
-import _toAirQualityIndex from "./toAirQualityIndex";
-export const toAirQualityIndex = _toAirQualityIndex;
+import _toAirQualityIndex from "./toAirQualityIndex"
+export const toAirQualityIndex = _toAirQualityIndex
