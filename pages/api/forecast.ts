176dfc7020350e665923ca5c7db14f6a1be1e4d6
dianyehezkiel <dianyehezkiel@gmail.com>
2022-08-07 01:01:54 +0700

    change api return value

--- pages/api/forecast.ts
+++ pages/api/forecast.ts
@@ -4,7 +4,10 @@ import { WeatherApiRequest, WeatherForecastFromApi } from "../../types"
 import { toWeatherForecast } from "../../utils"
 import { appid, BASE_URL } from "../../utils/constant"
 
-export default async function weatherForecast(req: WeatherApiRequest, res: NextApiResponse) {
+export default async function weatherForecast(
+  req: WeatherApiRequest,
+  res: NextApiResponse,
+) {
   try {
     const params = {
       lat: req.query.lat,
@@ -14,11 +17,12 @@ export default async function weatherForecast(req: WeatherApiRequest, res: NextA
       lang: 'id',
     }
 
-    const { data: forecastFromApi } = await axios.get<WeatherForecastFromApi>(`${BASE_URL}/forecast`, { params })
+    const { data: forecastFromApi } = await axios.get<WeatherForecastFromApi>(
+      `${BASE_URL}/forecast`,
+      { params },
+    )
 
-    res.json({
-      weatherForecast: toWeatherForecast(forecastFromApi),
-    })
+    res.json(toWeatherForecast(forecastFromApi))
   } catch (err) {
     res.status(500).json({ err })
   }
