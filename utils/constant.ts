89ea009e5e90adc112218b5e4afbbc0c717731fc
dianyehezkiel <dianyehezkiel@gmail.com>
2022-08-07 01:02:12 +0700

    add new constant

--- utils/constant.ts
+++ utils/constant.ts
@@ -1,2 +1,13 @@
 export const BASE_URL = 'https://api.openweathermap.org/data/2.5'
 export const appid = process.env.REACT_APP_API_KEY
+export const days = [
+  'Minggu',
+  'Senin',
+  'Selasa',
+  'Rabu',
+  'Kamis',
+  'Jum\'at',
+  'Sabtu',
+  'Hari ini',
+  'Besok',
+]
