a27b35ab1b8ad6c730a53165f9276b08739eac9b
dianyehezkiel <dianyehezkiel@gmail.com>
2022-08-07 12:44:57 +0700

    move request data from index (home) to component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,14 @@
 import { Box, Container } from '@chakra-ui/react'
 import Head from 'next/head'
-import CurrentWeather from "../components/CurrentWeather"
-import axios from "axios"
-import WeatherForecasts from "../components/WeatherForecasts"
+import CurrentWeather from '../components/CurrentWeather'
+import WeatherForecasts from '../components/WeatherForecasts'
 import AirQualityIndex from '../components/AirQualityIndex'
 import SearchBar from '../components/SearchBar'
-import { setAirQualityIndex, useStateValue } from '../state'
+import { useStateValue } from '../state'
 import React from 'react'
 
-export default function Home () {
-  const [
-    { coordinate, airQualityIndex },
-    dispatch,
-  ] = useStateValue()
-
-  React.useEffect(() => {
-    const params = {
-      lat: coordinate['default'].lat,
-      lon: coordinate['default'].lon,
-    }
-
-    const fetchData = async () => {
-      const { data } = await axios.get('/api/getWeatherData', { params })
-
-      dispatch(setAirQualityIndex(
-        "default",
-        data.airQualityIndex,
-      ))
-    }
-
-    fetchData()
-  }, [coordinate, dispatch])
+export default function Home() {
+  const [{ coordinate }] = useStateValue()
 
   return (
     <div>
@@ -39,39 +17,37 @@ export default function Home () {
         <meta
           name="description"
           content={
-            "Cuaca weather app by Dian Yehezkiel. Powered by Open Weather API."
+            'Cuaca weather app by Dian Yehezkiel. Powered by Open Weather API.'
           }
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Box as='main'
-        minH='100vh'
-        pt={16}
-        pb={24}
-        px={4}
-        bg='blue.300'
-      >
+      <Box as="main" minH="100vh" pt={16} pb={24} px={4} bg="blue.300">
         <Container
           p={0}
-          maxW='container.md'
-          display='flex'
-          flexDir='column'
-          alignItems='center'
+          maxW="container.md"
+          display="flex"
+          flexDir="column"
+          alignItems="center"
           gap={2}
         >
           <SearchBar />
           <CurrentWeather
-            id='default'
+            id="default"
             lat={coordinate['default'].lat}
             lon={coordinate['default'].lon}
           />
           <WeatherForecasts
-            id='default'
+            id="default"
             lat={coordinate['default'].lat}
             lon={coordinate['default'].lon}
           />
-          <AirQualityIndex data={airQualityIndex["default"]}/>
+          <AirQualityIndex
+            id="default"
+            lat={coordinate['default'].lat}
+            lon={coordinate['default'].lon}
+          />
         </Container>
       </Box>
     </div>
