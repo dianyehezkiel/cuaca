5467e74a1d90d4a3d39517bd7f9270a8907cb1b7
dianyehezkiel <dianyehezkiel@gmail.com>
2022-08-07 14:15:20 +0700

    fix bug

--- utils/constant.ts
+++ utils/constant.ts
@@ -1,5 +1,5 @@
 export const BASE_URL = 'https://api.openweathermap.org/data/2.5'
-export const appid = process.env.REACT_APP_API_KEY
+export const appid = process.env.API_KEY
 export const days = [
   'Minggu',
   'Senin',
