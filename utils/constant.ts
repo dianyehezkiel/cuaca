867d80a872d416f79053eb049f19a7645d285512
dianyehezkiel <dianyehezkiel@gmail.com>
2022-08-08 16:28:04 +0700

    add geocoding base url

--- utils/constant.ts
+++ utils/constant.ts
@@ -1,4 +1,5 @@
 export const BASE_URL = 'https://api.openweathermap.org/data/2.5'
+export const GC_BASE_URL = 'https://api.openweathermap.org/geo/1.0'
 export const appid = process.env.API_KEY
 export const days = [
   'Minggu',
