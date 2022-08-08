b6a89de52d94a4ecbd7f384b26330d56cdb676ef
dianyehezkiel <dianyehezkiel@gmail.com>
2022-08-08 16:27:24 +0700

    add GeoCoding types

---
+++ types/GeoCoding.ts
@@ -0,0 +1,14 @@
+import { Coordinate } from "./UniversalTypes"
+
+type LocalNames = {
+  [key: string]: string
+}
+
+export interface LocationFromApi extends Coordinate {
+  name: string;
+  local_names: LocalNames;
+  country: string;
+  state?: string;
+}
+
+export type LocationType = Omit<LocationFromApi, 'local_names'>
